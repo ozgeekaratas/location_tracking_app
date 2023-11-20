e9d817ae3fc03c1a63d91e06e50b6243afaa731c
ozgeekaratas <74623980+ozgeekaratas@users.noreply.github.com>
2023-11-20 13:24:59 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/location_tracking_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.location_tracking_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,214 @@
+import 'dart:async';
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/get_navigation.dart';
+
+import 'package:location/location.dart' as l;
+import 'package:permission_handler/permission_handler.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const GetMaterialApp(
+      themeMode: ThemeMode.system,
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  bool gpsEnabled = false;
+  bool permissionGranted = false;
+  l.Location location = l.Location();
+  late StreamSubscription subscription;
+  bool trackingEnabled = false;
+
+  List<l.LocationData> locations = [];
+
+  @override
+  void initState() {
+    super.initState();
+    checkStatus();
+  }
+
+  @override
+  void dispose() {
+    stopTracking();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Location App'),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 12),
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            buildListTile(
+              "GPS",
+              gpsEnabled
+                  ? const Text("Okey")
+                  : ElevatedButton(
+                      onPressed: () {
+                        requestEnableGps();
+                      },
+                      child: const Text("Enable Gps")),
+            ),
+            buildListTile(
+              "Permission",
+              permissionGranted
+                  ? const Text("Okey")
+                  : ElevatedButton(
+                      onPressed: () {
+                        requestLocationPermission();
+                      },
+                      child: const Text("Request Permission")),
+            ),
+            buildListTile(
+              "Location",
+              trackingEnabled
+                  ? ElevatedButton(
+                      onPressed: () {
+                        stopTracking();
+                      },
+                      child: const Text("Stop"))
+                  : ElevatedButton(
+                      onPressed: gpsEnabled && permissionGranted
+                          ? () {
+                              startTracking();
+                            }
+                          : null,
+                      child: const Text("Start")),
+            ),
+            Expanded(
+                child: ListView.builder(
+              itemCount: locations.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(
+                      "${locations[index].latitude} ${locations[index].longitude}"),
+                );
+              },
+            ))
+          ],
+        ),
+      ),
+    );
+  }
+
+  ListTile buildListTile(
+    String title,
+    Widget? trailing,
+  ) {
+    return ListTile(
+      dense: true,
+      title: Text(title),
+      trailing: trailing,
+    );
+  }
+
+  void requestEnableGps() async {
+    if (gpsEnabled) {
+      log("zaten açık");
+    } else {
+      bool isGpsActive = await location.requestService();
+      if (!isGpsActive) {
+        setState(() {
+          gpsEnabled = false;
+        });
+        log("kullanıcı gps i açmadı");
+      } else {
+        log("kullanıcı izin verdi ve açtı");
+        setState(() {
+          gpsEnabled = true;
+        });
+        // checkGpsStatus();
+      }
+    }
+  }
+
+  void requestLocationPermission() async {
+    PermissionStatus permissionStatus =
+        await Permission.locationWhenInUse.request();
+    if (permissionStatus == PermissionStatus.granted) {
+      setState(() {
+        permissionGranted = true;
+      });
+    } else {
+      setState(() {
+        permissionGranted = false;
+      });
+    }
+  }
+
+  Future<bool> isPermissionGranted() async {
+    return await Permission.locationWhenInUse.isGranted;
+  }
+
+  Future<bool> isGpsEnabled() async {
+    return await Permission.location.serviceStatus.isEnabled;
+  }
+
+  checkStatus() async {
+    bool _permissionGranted = await isPermissionGranted();
+    bool _gpsEnabled = await isGpsEnabled();
+    setState(() {
+      permissionGranted = _permissionGranted;
+      gpsEnabled = _gpsEnabled;
+    });
+  }
+
+  addLocation(l.LocationData data) {
+    setState(() {
+      locations.insert(0, data); //Listenin en başına ekler.
+    });
+  }
+
+  clearLocation() {
+    setState(() {
+      locations.clear();
+    });
+  }
+
+  void startTracking() async {
+    if (!(await isGpsEnabled())) {
+      return;
+    }
+    if (!(await isPermissionGranted())) {
+      return;
+    }
+    subscription = location.onLocationChanged.listen((event) {
+      addLocation(event);
+    });
+    setState(() {
+      trackingEnabled = true;
+    });
+  }
+
+  void stopTracking() {
+    subscription.cancel();
+    setState(() {
+      trackingEnabled = false;
+    });
+    clearLocation();
+  }
+}
